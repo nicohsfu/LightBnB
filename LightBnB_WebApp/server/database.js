beedbce556b33b46a4964a5e1a7bd19f4f32a89c
nicohsfu <marcnicohernandez@gmail.com>
2022-10-20 12:10:11 -0700

    refactored get all reservations function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -101,9 +101,39 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+  SELECT reservations.*, properties.*, AVG(rating) as average_rating
+  FROM reservations 
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id , reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+
+// SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+// FROM reservations
+// JOIN properties ON reservations.property_id = properties.id
+// JOIN property_reviews ON properties.id = property_reviews.property_id
+// WHERE reservations.guest_id = 1
+// GROUP BY properties.id, reservations.id
+// ORDER BY reservations.start_date
+// LIMIT 10;
+
+  return pool
+    .query(query, [guest_id, limit])
+    .then((result) => result.rows
+    )
+    .catch(() => {
+      return null;
+    });
 };
 exports.getAllReservations = getAllReservations;
+
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
 
 /// Properties
 
